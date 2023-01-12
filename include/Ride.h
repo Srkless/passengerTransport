891b42b78661dd7405d42f5d2bcfda67a8e051c5
Chameleeon <danichelseae3@gmail.com>
2023-01-12 12:11:31 +0100

    Bugfix.

--- include/Ride.h
+++ include/Ride.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
-#include <chrono>
+#include <filesystem>
+#include <sstream>
 #include <vector>
 
 class Ride
@@ -8,28 +9,28 @@ class Ride
 private:
 	std::string m_RideID;
 	std::string m_Driver;
-	std::chrono::system_clock::time_point m_StartTime;
-	std::chrono::system_clock::time_point m_EndTime;
+	std::string m_StartTime;
+	std::string m_EndTime;
 	std::string m_StartLocation;
 	std::vector<std::string> m_PathLocations;
 	std::string m_EndLocation;
 public:
 	Ride();
-	Ride(std::string rideID, std::string driver, std::chrono::system_clock::time_point startTime,
-		std::chrono::system_clock::time_point endTime, std::string startLocation, std::vector<std::string> pathLocations, std::string endLocation);
+	Ride(std::string rideID, std::string driver, std::string startTime,
+		std::string endTime, std::string startLocation, std::vector<std::string> pathLocations, std::string endLocation);
 
 	void setRideID(std::string RideID);
 	void setDriver(std::string driver);
-	void setStartTime(std::chrono::system_clock::time_point startTime);
-	void setEndTime(std::chrono::system_clock::time_point endTime);
+	void setStartTime(std::string startTime);
+	void setEndTime(std::string endTime);
 	void setStartLocation(std::string startLocation);
 	void setEndLocation(std::string endLocation);
 	void setPathLocationos(std::vector<std::string> pathLocations);
 
 	std::string getRideID() const;
 	std::string getDriver() const;
-	std::chrono::system_clock::time_point getStartTime() const;
-	std::chrono::system_clock::time_point getEndTime() const;
+	std::string getStartTime() const;
+	std::string getEndTime() const;
 	std::string getStartLocation() const;
 	std::vector<std::string> getPathLocations() const;
 	std::string geEndLocation() const;
--- source/Ride.cpp
+++ source/Ride.cpp
@@ -1,9 +1,9 @@
 #include "Ride.h"
 
 Ride::Ride()
-	: m_Driver(""), m_StartTime(std::chrono::system_clock::now()), m_EndTime(std::chrono::system_clock::now()), m_StartLocation(""), m_PathLocations{}, m_EndLocation("") {}
+	: m_Driver(""), m_StartTime(std::string()), m_EndTime(std::string()), m_StartLocation(""), m_PathLocations{}, m_EndLocation("") {}
 
-Ride::Ride(std::string rideID, std::string driver, std::chrono::system_clock::time_point startTime, std::chrono::system_clock::time_point endTime, std::string startLocation, std::vector<std::string> pathLocations, std::string endLocation)
+Ride::Ride(std::string rideID, std::string driver, std::string startTime, std::string endTime, std::string startLocation, std::vector<std::string> pathLocations, std::string endLocation)
 	: m_RideID(rideID), m_Driver(driver), m_StartTime(startTime), m_EndTime(endTime), m_StartLocation(startLocation), m_PathLocations(pathLocations), m_EndLocation(endLocation) {}
 
 void Ride::setRideID(std::string RideID)
@@ -16,12 +16,12 @@ void Ride::setDriver(std::string driver)
 	m_Driver = driver;
 }
 
-void Ride::setStartTime(std::chrono::system_clock::time_point startTime)
+void Ride::setStartTime(std::string startTime)
 {
 	m_StartTime = startTime;
 }
 
-void Ride::setEndTime(std::chrono::system_clock::time_point endTime)
+void Ride::setEndTime(std::string endTime)
 {
 	m_EndTime = endTime;
 }
@@ -51,12 +51,12 @@ std::string Ride::getDriver() const
 	return m_Driver;
 }
 
-std::chrono::system_clock::time_point Ride::getStartTime() const
+std::string Ride::getStartTime() const
 {
 	return m_StartTime;
 }
 
-std::chrono::system_clock::time_point Ride::getEndTime() const
+std::string Ride::getEndTime() const
 {
 	return m_EndTime;
 }
@@ -81,40 +81,28 @@ std::istream& operator>>(std::istream& is, Ride& ride)
 	std::string line;
 	std::getline(is, line);
 	std::stringstream sstream(line);
-	std::time_t timeItem;
 	std::vector<std::string> items;
-	std::vector<time_t> timeItems;
-	std::string item;
 	std::vector<std::string> pathLocationItems;
+	std::string item;
 	size_t count = 0;
-	while (std::getline(sstream, item, '#') && count < 2)
+	while (std::getline(sstream, item, '#') && count < 5)
 	{
 		items.push_back(item);
 		count++;
 	}
-	while (std::getline(sstream, timeItem, '#') && count < 4)
-	{
-		timeItems.push_back(timeItem);
-		count++;
-	}
-
-	std::getline(sstream, item, '#');
-	items.push_back(item);
-
 	while (std::getline(sstream, item, '!'))
 	{
 		pathLocationItems.push_back(item);
 	}
 
 	std::getline(sstream, item, '#');
-	items.push_back(item);
 
 	ride.m_RideID = items[0];
 	ride.m_Driver = items[1];
-	ride.m_StartTime = timeItems[0];
-	ride.m_EndTime = timeItems[1];
-	ride.m_StartLocation = timeItems[2];
-	ride.m_EndLocation = timeItems[3];
+	ride.m_StartTime = items[2];
+	ride.m_EndTime = items[3];
+	ride.m_StartLocation = items[4];
+	ride.m_EndLocation = item;
 	ride.m_PathLocations = pathLocationItems;
 
 	return is;
