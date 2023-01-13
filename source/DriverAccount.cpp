d686003c65420629b74407520c8f01fca1a2d717
DESKTOP-1C7EODV\WIN10 <igorpiljagic1@gmail.com>
2023-01-13 22:48:32 +0100

    bug fixes

--- source/DriverAccount.cpp
+++ source/DriverAccount.cpp
@@ -157,3 +157,15 @@ void DriverAccount::driveRoute(const std::string& fileName) // prima RideID (ime
 		db::rewriteExistingRide(driverMap[fileName]);
 	}
 }
+
+bool DriverAccount::checkRouteAndReport() const
+{
+	std::unordered_map<std::string, Ride> undrivenRidesMap = allUndrivenRides();
+
+	std::unordered_map<std::string, Report> reportMap = db::loadDriverReports(getUsername());
+
+	if (undrivenRidesMap.size() != reportMap.size())
+		return false;
+	else
+		return true;
+}
