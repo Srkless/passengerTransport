f35396585b64bac3921e648c280ae26f9450534c
Chameleeon <danichelsea3@gmail.com>
2023-01-17 20:46:19 +0100

    Bugfix. Program would crash when trying to read from an empty file while creating a travel warrant. Added feature to main where if the program crashes, the data is saved as a backup and the program is reset with only the accounts are kept.

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "gui.h"
+#include <csignal>
 #include <fstream>
 #include <filesystem>
 #include "utility.h"
@@ -9,16 +10,55 @@
 #include "Report.h"
 #include "ProblemReport.h"
 #include "Ride.h"
+#include <cstdio>
+#include <exception>
+#include <cassert>
+
+void resetProgram()
+{
+	std::filesystem::path path = std::filesystem::current_path();
+	path += "\\crash_backup\\";
+	db::createSafetyCopy(path);
+	remove("data");
+	std::filesystem::create_directories("data");
+	path += "data\\users";
+	std::filesystem::path copyDestination("data");
+	std::filesystem::copy(path, copyDestination, std::filesystem::copy_options::recursive);
+
+	char* argv[3];
+	argv[0] = strdup("softver-za-prevoz-putnika");
+	argv[1] = NULL;
+	argv[2] = NULL;
+	execvp(argv[0], argv);
+}
+
+void breakpoint_handler(int signum)
+{
+	throw std::exception("Program Crashed Unexpectedly!");
+}
+
+void assertion_handler(int sig)
+{
+	throw std::exception("Program Crashed Unexpectedly!");
+}
+
 
 int main()
 {
-	config::configure();
-	////Administrator admin;
-	////admin.CreateCodeBook();
-	////admin.CreateCodeBook();
-	//////admin.SuspendingUserAccount();
-	////admin.ViewUserAccount();
-	gui::loginInterface();
-	//gui::DriverInterface()
+	// IN CASE THE PROGRAM CRASHES, DELETE THE DATA FOLDER, CREATE A SAFETY BACKUP AND RESTART THE PROGRAM
+
+	try
+	{
+		config::configure();
+		gui::loginInterface();
+		signal(SIGILL, breakpoint_handler);
+		signal(SIGABRT, assertion_handler);
+		assert(false);
+	}
+	catch (...)
+	{
+		resetProgram();
+		return 0;
+	}
 	return 0;
 }
